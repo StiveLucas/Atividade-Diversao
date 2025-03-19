0c914e28c88d2311c64139181480c5b3d927a58f
StiveLucas <161142347+StiveLucas@users.noreply.github.com>
2025-03-19 12:40:34 +0000

    Tutorial

---
+++ AtividadeDeExcecao.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class AtividadeDeExcecao {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Digite o X: ");
+            int x = sc.nextInt();
+
+            System.out.println("Digite o valor de Y: ");
+            int y = sc.nextInt();
+
+            System.out.println("Resultado da divisão: "+ x / y);
+            
+        } catch (InputMismatchException e) {
+            System.out.println("Formato inválido! \nDigite um valor inteiro.");
+
+        }catch(ArithmeticException e){
+            System.out.println("Não é possivel fazer essa divisão.");
+
+
+        }
+
+    }
+    
+}
